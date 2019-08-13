c1751588c5463cb4f414857c1e5d0b728d277f98
mpragnarok <mp922352612@gmail.com>
2019-08-13 22:09:45 +0800

    update index.js for mouse click to playsound

--- 01_JavaScript-Drum-Kit/index.js
+++ 01_JavaScript-Drum-Kit/index.js
@@ -1,9 +1,10 @@
 const keys = document.querySelectorAll('.key')
 
+// mouse click on element or keyboard pressed to playsound
 function playsound(e) {
-  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
-  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
-
+  let keyCode = e.keyCode || this.dataset.key
+  const audio = document.querySelector(`audio[data-key="${keyCode}"]`)
+  const key = document.querySelector(`.key[data-key="${keyCode}"]`)
   if (!audio) return // stop the function from running all together
   audio.currentTime = 0 // rewind the audio
   audio.play()
@@ -20,3 +21,6 @@ keys.forEach(key => key.addEventListener('transitionend', removeTransition))
 
 // bind an event listener to window for keyboard and play the audio
 window.addEventListener('keydown', playsound)
+
+// bind an event listener to keys
+keys.forEach(key => key.addEventListener('click', playsound))
